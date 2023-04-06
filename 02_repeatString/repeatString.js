27525f7cd25a96d55711e45be17224c815aa2854
Arman <arman.zattar@gmail.com>
2023-04-06 19:06:54 -0400

    Finish second test exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let newWord = ''
+    if (number < 0) {
+        newWord = 'ERROR'
+    } else if (number == 0 || word == '') {
+        newWord = ''
+    } else {
+        for (let i = 0; i < number; i++) {
+            newWord = newWord + word
+        }
+    }
+    return newWord
 };
 
 // Do not edit below this line
