0442d3940033e2aef61c67ed8bc490264fa09122
Arman <arman.zattar@gmail.com>
2023-04-06 19:30:52 -0400

    Finish fifth test exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if ((!Number.isInteger(number1) || number1 < 0) || (!Number.isInteger(number2) || number2 < 0)) {
+        return 'ERROR'
+    } else {
+        let first = number1 > number2 ? number2 : number1
+        let last = number1 == first ? number2 : number1
+        let sum = first;
+        for (let i = first+1; i <= last; i++) {
+            sum += i
+        }
+        return sum
+    }
 };
 
 // Do not edit below this line
