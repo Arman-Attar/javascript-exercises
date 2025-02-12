088bb076d42cb11a201dfbde51b46379de48feb9
Arman <arman.zattar@gmail.com>
2025-02-12 00:13:41 -0500

    finished exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let newWord = ""
+for (let index = 0; index < times; index++) {
+    newWord += word
+}
+return newWord
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+const charSet = input.split('');
+let reverseString = ""
+for (let index = charSet.length-1; index >= 0; index--){
+    reverseString += charSet[index]
+}
+return reverseString
 };
-
 // Do not edit below this line
 module.exports = reverseString;
