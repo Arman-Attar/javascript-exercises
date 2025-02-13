7eca8f4a26662344239004ac49f1f6126e58ac13
Arman <arman.zattar@gmail.com>
2025-02-12 22:19:06 -0500

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let sum = 0
+    if (!Number.isInteger(arg1) || !Number.isInteger(arg2) || arg1 < 0 || arg2 < 0) {
+        return "ERROR"
+    } else {
+    let first, second = 0;
+    if (arg1 > arg2) { 
+        first = arg2
+        second = arg1
+    } else {
+        first = arg1
+        second = arg2
+    }
+    for (let index = first; index <= second; index++){
+        sum += index
+    }
+    return sum
+}
 };
 
 // Do not edit below this line
