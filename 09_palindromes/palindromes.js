cc8b4cb8353b928c0391180014362f732a25ab51
Arman <arman.zattar@gmail.com>
2025-02-15 15:04:34 -0500

    finished exercises 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const allowed = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
+const palindromes = function (string) {
+
+const newString = string
+.toLowerCase()
+.split('')
+.filter((char) => allowed.includes(char))
+.join('')
+
+reversedString = newString.split('').reverse().join('')
+
+return newString == reversedString
 };
 
 // Do not edit below this line
