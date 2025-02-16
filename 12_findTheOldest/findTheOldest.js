96522dfc91edf3f51fb68d69db64301c28c9b38a
Arman <arman.zattar@gmail.com>
2025-02-15 20:05:20 -0500

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+return people.reduce((ac, person) => {
+    if (getAge(ac.yearOfDeath, ac.yearOfBirth) < getAge(person.yearOfDeath, person.yearOfBirth)) {
+        ac = person
+    }
+    return ac
+})
 
 };
 
+
+function getAge(death, birth) {
+    if (isNaN(death)){
+        return (new Date().getFullYear()) - birth
+    } 
+    return death - birth
+}
 // Do not edit below this line
 module.exports = findTheOldest;
