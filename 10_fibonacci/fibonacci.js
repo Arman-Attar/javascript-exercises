d63bb8e1fc6dfdc06f11e46a784615a0be352f5a
Arman <arman.zattar@gmail.com>
2025-02-15 19:08:17 -0500

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+let convertedNumber = parseInt(number)
+if (convertedNumber == 0) return 0
+if (convertedNumber < 0) {
+    return "OOPS"
+}
+let first = 1
+let second = 0
 
+for (let i = 2; i <= convertedNumber; i++){
+    let current = first + second
+    second = first
+    first = current
+}
+return first
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
